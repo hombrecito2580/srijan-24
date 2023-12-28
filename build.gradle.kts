2bdf8a0a56b21c17c2feea58097466e87892c7f9
Pratyush Kumar Chaturvedi <hombrecito2580@gmail.com>
2023-12-28 22:08:26 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}

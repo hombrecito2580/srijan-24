40fab0c839484bcf0aa2cb34a29d8e26511ee2d3
Siddharth <goraisiddharth@gmail.com>
2024-01-12 15:15:27 +0530

    profile ui done

--- app/src/main/java/com/iitism/srijan24/ui/MainActivity.kt
+++ app/src/main/java/com/iitism/srijan24/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.iitism.srijan24.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -47,6 +49,13 @@ class MainActivity : AppCompatActivity(), PaymentResultListener {
 //        setupActionBarWithNavController(navController, binding.drawerLayout)
 //        binding.navView.setupWithNavController(navController)
 
+        if(navController.currentDestination?.id == R.id.homeFragment){
+            binding.appBar.btnProfile.visibility = View.VISIBLE
+            binding.appBar.btnProfile.setOnClickListener {
+                navController.navigate(R.id.action_homeFragment_to_profileFragment)
+            }
+        }
+
 
         binding.appBar.btnMenu.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
@@ -76,8 +85,12 @@ class MainActivity : AppCompatActivity(), PaymentResultListener {
                 R.id.aboutUsFragment -> "About Us"
                 R.id.coreTeamFragment -> "Core Team"
                 R.id.contactFragment -> "Contact Us"
+                R.id.profileFragment -> "Profile"
                 else -> "Srijan 24"
             }
+
+            if(destination.id != R.id.homeFragment) binding.appBar.btnProfile.visibility = View.GONE
+            else binding.appBar.btnProfile.visibility = View.VISIBLE
         }
 
         binding.navView.setupWithNavController(navController)
---
+++ app/src/main/java/com/iitism/srijan24/ui/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.iitism.srijan24.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.iitism.srijan24.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}

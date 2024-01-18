adab151b1b97ae25f23dea706ebf427ece0fdaf3
SaurikSaha <rick.iitian@gmail.com>
2024-01-18 14:49:05 +0530

    EventsRegnUI

--- app/src/main/java/com/iitism/srijan24/retrofit/EventsApi.kt
+++ app/src/main/java/com/iitism/srijan24/retrofit/EventsApi.kt
@@ -3,8 +3,13 @@ package com.iitism.srijan24.retrofit
 import com.iitism.srijan24.data.EventDataModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.SimpleTimeZone
 
 interface EventsApi {
-    @GET("/showAllEvents")
-    fun getAllEvents(): Call<List<EventDataModel>>
+    @GET("/events/{zone}")
+    fun getAllEvents(
+        @Query("zone")
+        zone: String
+    ): Call<List<EventDataModel>>
 }

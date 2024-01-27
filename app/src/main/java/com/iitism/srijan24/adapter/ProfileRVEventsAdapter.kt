9b793b5096aa6a35c296cc0ce1104f0cfc1e6bf6
Siddharth <goraisiddharth@gmail.com>
2024-01-27 23:48:39 +0530

    events fully functional

--- app/src/main/java/com/iitism/srijan24/adapter/ProfileRVEventsAdapter.kt
+++ app/src/main/java/com/iitism/srijan24/adapter/ProfileRVEventsAdapter.kt
@@ -1,5 +1,6 @@
 package com.iitism.srijan24.adapter
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,14 +12,19 @@ import com.iitism.srijan24.R
 import com.iitism.srijan24.data.GetUserEventsResponse
 import com.iitism.srijan24.data.GetUserEventsResponseItem
 
-class ProfileRVEventsAdapter(private var dataList: ArrayList<GetUserEventsResponseItem>): RecyclerView.Adapter<ProfileRVEventsAdapter.EventsViewHolder>() {
-    inner class EventsViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class ProfileRVEventsAdapter(
+    private var dataList: ArrayList<GetUserEventsResponseItem>,
+    private val userEmailEvents: String
+) : RecyclerView.Adapter<ProfileRVEventsAdapter.EventsViewHolder>() {
+    inner class EventsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvEventName: TextView = view.findViewById(R.id.tvEvent)
         val tvTeamName: TextView = view.findViewById(R.id.tvTeamName)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.registered_events_card, parent, false)
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.registered_events_card, parent, false)
         return EventsViewHolder(itemView)
     }
 
@@ -27,13 +33,29 @@ class ProfileRVEventsAdapter(private var dataList: ArrayList<GetUserEventsRespon
     }
 
     override fun onBindViewHolder(holder: EventsViewHolder, position: Int) {
+
         val currentData = dataList[position]
         Log.d("abcd", position.toString())
 
-            holder.tvEventName.text = "Events: ${currentData.eventName}"
-        if(currentData.teams[0].TeamName.isNullOrEmpty()){
-            holder.tvTeamName.visibility=View.INVISIBLE
-        }else
-            holder.tvTeamName.text = "Team Name: ${currentData.teams[0].TeamName}"
+        holder.tvEventName.text = "Events: ${currentData.eventName}"
+        var teamName = ""
+//        Log.d("Current Data",currentData.toString())
+        for (i in currentData.teams) {
+            for(j in i.MembersList){
+                if(j.Email == userEmailEvents){
+                    teamName = i.TeamName.toString()
+                }
+            }
+            Log.d("TAG", i.toString())
+        }
+
+//        if (currentData.teams[0].TeamName.isNullOrEmpty()) {
+//            holder.tvTeamName.visibility = View.INVISIBLE
+//        } else
+//            holder.tvTeamName.text = "Team Name: ${currentData.teams[0].TeamName}"
+        if (teamName.isEmpty()) {
+            holder.tvTeamName.visibility = View.INVISIBLE
+        } else
+            holder.tvTeamName.text = "Team Name: ${teamName}"
     }
 }
